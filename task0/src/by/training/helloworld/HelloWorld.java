476f0dece816ab393f899972c1923dd182d17cb7
Evgeniy Filippovich <filippovich.z@mail.ru>
2019-07-02 23:22:28 +0300

    Hello world

---
+++ task0/src/by/training/helloworld/HelloWorld.java
@@ -0,0 +1,28 @@
+package by.training.helloworld;
+
+import org.apache.log4j.Logger;
+
+public final class HelloWorld {
+    /**
+     * @serial Logger.
+     */
+    private static final Logger LOGGER = Logger.getLogger(HelloWorld.class);
+
+    /**
+     * Default constructor.
+     */
+    private HelloWorld() {
+    }
+
+    /**
+     * @param args command line.
+     */
+    public static void main(final String[] args) {
+        print("Hello world!!!");
+        print("Hello java!!!");
+    }
+
+    private static void print(final Object object) {
+        LOGGER.info(object);
+    }
+}
