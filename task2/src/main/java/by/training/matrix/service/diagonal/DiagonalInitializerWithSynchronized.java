234cf05a7ed8e4b486c58d6543c57784f2a110a9
Evgeniy Filippovich <filippovich.z@mail.ru>
2019-08-11 18:17:55 +0300

    add synchronized method

---
+++ task2/src/main/java/by/training/matrix/service/diagonal/DiagonalInitializerWithSynchronized.java
@@ -0,0 +1,47 @@
+package by.training.matrix.service.diagonal;
+
+import by.training.matrix.bean.Matrix;
+
+public class DiagonalInitializerWithSynchronized {
+    /**
+     * Matrix.
+     */
+    private Matrix matrix;
+    /**
+     * Count of threads.
+     */
+    private int countThreads;
+
+    /**
+     * @param matrixNew       matrix.
+     * @param countThreadsNew count of threads.
+     */
+    public DiagonalInitializerWithSynchronized(
+            final Matrix matrixNew, final int countThreadsNew) {
+        matrix = matrixNew;
+        countThreads = countThreadsNew;
+    }
+
+    /**
+     * Initialize matrix.
+     */
+    public void initializeMatrix() {
+        int countElements = matrix.getCountRows() / countThreads;
+        int additional = matrix.getCountRows() % countThreads;
+        Thread[] threads = new Thread[countThreads];
+        int temp = 1;
+        for (int i = 0; i < countThreads; ++i) {
+            if (additional == 0) {
+                temp = 0;
+            } else {
+                --additional;
+            }
+            int count = countElements + temp;
+            threads[i] = new Thread(new DiagonalTaskWithSynchronized(matrix,
+                    i + 1, count));
+        }
+        for (int i = 0; i < countThreads; ++i) {
+            threads[i].start();
+        }
+    }
+}
---
+++ task2/src/main/java/by/training/matrix/service/diagonal/DiagonalTaskWithSynchronized.java
@@ -0,0 +1,66 @@
+package by.training.matrix.service.diagonal;
+
+import by.training.matrix.bean.Matrix;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.concurrent.TimeUnit;
+
+public class DiagonalTaskWithSynchronized implements Runnable {
+    /**
+     * Matrix.
+     */
+    private Matrix matrix;
+    /**
+     * Number element.
+     */
+    private int number;
+    /**
+     * Count elements for thread.
+     */
+    private int countElements;
+    /**
+     * Logger.
+     */
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    /**
+     * @param matrixNew        matrix.
+     * @param numberNew        number-element.
+     * @param countElementsNew count elements.
+     */
+
+    DiagonalTaskWithSynchronized(final Matrix matrixNew,
+                                 final int numberNew,
+                                 final int countElementsNew) {
+        matrix = matrixNew;
+        number = numberNew;
+        countElements = countElementsNew;
+    }
+
+    /**
+     * run.
+     */
+    @Override
+    public void run() {
+        int temp = 0;
+        final int time = 50;
+        for (int i = 0; i < matrix.getCountRows(); ++i) {
+            synchronized (DiagonalTaskWithSynchronized.class) {
+                if (matrix.getElement(i, i) == 0) {
+                    matrix.setElement(i, i, number);
+                    ++temp;
+                }
+            }
+            try {
+                TimeUnit.MILLISECONDS.sleep(time);
+            } catch (InterruptedException eNew) {
+                LOGGER.error(eNew);
+                Thread.currentThread().interrupt();
+            }
+            if (temp == countElements) {
+                break;
+            }
+        }
+    }
+}
