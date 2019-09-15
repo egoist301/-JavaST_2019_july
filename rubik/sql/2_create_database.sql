56b42c14c0e4d0ad4dac811cc41ecbab57b8e03e
Evgeniy Filippovich <filippovich.z@mail.ru>
2019-09-15 07:53:51 +0300

    fix

--- rubik/sql/2_create_database.sql
+++ rubik/sql/2_create_database.sql
@@ -1,11 +1,7 @@
 CREATE DATABASE `rubik` DEFAULT CHARACTER SET utf8;
 
-CREATE USER `rubik_user`@`localhost`;
-
-GRANT SELECT, UPDATE, INSERT, DELETE
-ON `rubik`.*
-TO rubik_user@localhost;
+CREATE USER `rubik_user`@`localhost` IDENTIFIED BY 'rubik_password';
 
 GRANT SELECT, UPDATE, INSERT, DELETE
     ON `rubik`.*
-    TO rubik_user@'%';
+    TO rubik_user@localhost;
