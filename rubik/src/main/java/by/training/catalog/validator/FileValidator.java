5bef053f4e607aa4dd777fa22460c84c7710f724
Evgeniy Filippovich <filippovich.z@mail.ru>
2019-09-22 16:06:27 +0300

    Add proxy connection and add file validator

---
+++ rubik/src/main/java/by/training/catalog/validator/FileValidator.java
@@ -0,0 +1,35 @@
+package by.training.catalog.validator;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+
+/**
+ * A validation class that can be used to check if file with chosen name exists and is not a directory.
+ */
+public final class FileValidator {
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    /**
+     * Checks if file with chosen name exists and is not directory.
+     *
+     * @param filename name of the file
+     * @return {@code true} is file exists and is not directory. If filename is null, returns {@code false}
+     */
+    public boolean validate(final String filename) {
+
+        if (filename == null) {
+            LOGGER.warn(
+                    "filename argument is null, validation result is false");
+            return false;
+        }
+        LOGGER.debug(filename, "argument ");
+        File file = new File(filename);
+        boolean result = file.exists() && file.isFile();
+        String info = filename + " validation result is " + result;
+        LOGGER.info(info);
+        return result;
+    }
+}
